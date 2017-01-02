09472681eac4be4c954325471d7268cdedb25fa0
Vikharau-Yahor <vikharau@mail.ru>
2017-01-02 22:38:00 +0300

    commit

---
+++ Test/Console_DZ-Test/Console_DZ-Test/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_DZ_Test
+{
+    class Class1
+    {
+        public int num
+        {
+            get { return num; }
+            set
+            {
+                if (value == 4)
+                    num = value;
+            }
+        }
+
+        public int Get()
+        {
+            return Get();
+        }
+        public void Set(int value)
+        {
+            if (value == 4)
+                Set(value);
+        }
+    }
+}
---
+++ Test/Console_DZ-Test/Console_DZ-Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_DZ_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            Console.Write(obj.num.ToString());
+        }
+    }
+}
